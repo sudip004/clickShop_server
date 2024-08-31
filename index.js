afd11dbf4c0b806012df64e6de0041237f9470e8
Sudip Basak <84569249+sudip004@users.noreply.github.com>
2024-08-31 14:55:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ const NodeCache = require("node-cache"); // Import NodeCache
 // Initialize cache
 const cache = new NodeCache({ stdTTL: 60 * 60 }); // Cache TTL is set to 1 hour
 
-const URL = "https://click-shop-client-lilac.vercel.app";
-// const URL = "http://localhost:5173";
+// const URL = "https://click-shop-client-lilac.vercel.app";
+const URL = "http://localhost:5173";
 
 // Cloudinary configuration
 cloudinary.config({
@@ -30,7 +30,7 @@ cloudinary.config({
 
 app.use(express.json());
 const corsOptions = {
-    origin: [URL, "http://localhost:3000","http://localhost:5173"],
+    origin: [URL, "http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"], // Include all the HTTP methods you use
     credentials: true, // Allows cookies to be sent in cross-origin requests
     allowedHeaders: ["Content-Type", "Authorization"], // Include any custom headers you use
