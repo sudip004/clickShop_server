87b695181938558ea3f0a26c5a6f1562651a7b3a
Sudip Basak <84569249+sudip004@users.noreply.github.com>
2024-08-31 12:20:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ cloudinary.config({
 
 app.use(express.json());
 app.use(cors({
-    origin: [URL , "http://localhost:3000"],
+    origin: [URL , "http://localhost:3000","https://click-shop-client-seven.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true
 }));
